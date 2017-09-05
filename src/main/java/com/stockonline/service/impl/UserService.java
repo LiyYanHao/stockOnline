4da16e91a75e8307f76d89bff532442233bfe349
lyh <1779077919@qq.com>
2017-09-05 16:42:15 +0800

    用户注册入库信息

---
+++ src/main/java/com/stockonline/service/impl/UserService.java
@@ -0,0 +1,34 @@
+package com.stockonline.service.impl;
+
+import com.stockonline.mapper.UserDao;
+import com.stockonline.util.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * Created by lyh on 17-9-5.
+ */
+@Service
+public class UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    public void insertUserInfo(Map<String,String> info){
+        info.put("userRegDate", DateUtils.getNowDateTime());//添加注册时间
+        info.put("username","游客"+info.get("email").substring(0,info.get("email").indexOf("@")));
+        userDao.insertUserInfo(info);
+    }
+
+    /**
+     * 根据email查询用户信息并存入session
+     * @param email
+     * @return
+     */
+    public Map<String,String> getUserInfo(String email){
+
+        return null;
+    }
+}
