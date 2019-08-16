0b605b43a1bb4140da38c197f373df9519e588a9
lyh <liyanhao9999@outlook.com>
2019-08-16 18:26:08 +0800

    springboot enable注解驱动开发

--- src/main/java/com/learn/springBoot/springbootCoreThinking/Chapter9_2_4_ImportListener/DefaultAutoConfigurationImportListener.java
+++ src/main/java/com/learn/springBoot/springbootCoreThinking/Chapter9_2_4_ImportListener/DefaultAutoConfigurationImportListener.java
@@ -21,15 +21,15 @@ public class DefaultAutoConfigurationImportListener implements AutoConfiguration
         // 获取当前 ClassLoader
         ClassLoader classLoader = autoConfigurationImportEvent.getClass().getClassLoader();
         // 候选的自动装配类名单
-        List<EnableAutoConfiguration> enableAutoConfigurations =
-                SpringFactoriesLoader.loadFactories(EnableAutoConfiguration.class, classLoader);
+//        List<EnableAutoConfiguration> s =
+//                SpringFactoriesLoader.loadFactories(EnableAutoConfiguration.class, classLoader);
         // 实际的自动装配类名单
         List<String> candidateConfigurations = autoConfigurationImportEvent.getCandidateConfigurations();
         // 排除的自动装配类名单
         Set<String> exclusions = autoConfigurationImportEvent.getExclusions();
         // 输出各自数量
-        System.out.printf("自动装配类名单 - 候选数量：%d，实际数量：%d，排除数量：%s\n",
-                enableAutoConfigurations.size(), candidateConfigurations.size(), exclusions.size());
+        //System.out.printf("自动装配类名单 - 候选数量：%d，实际数量：%d，排除数量：%s\n",
+                //enableAutoConfigurations.size(), candidateConfigurations.size(), exclusions.size());
         // 输出实际和排除的自动装配类名单
         System.out.println("实际的自动装配类名单：");
         autoConfigurationImportEvent.getCandidateConfigurations().forEach(System.out::println);
